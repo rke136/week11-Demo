d30f061b56062dc070346cec0bf592d4411c7b0e
Julia Vrn <julia-vrn@hotmail.com>
2022-04-08 14:29:58 +0200

    update search page

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/search', (req, res) => {
